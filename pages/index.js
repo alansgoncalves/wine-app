c431d4cd5f43d3af64a4e61ea7c149c62a45f5c0
alansgoncalves <vekio.soft@gmail.com>
2022-07-03 18:05:08 -0300

    feat: add fetch API and render products datain front

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,77 @@
-import Link from '../src/components/Link/NextLink'
-import { Header, HeaderImages } from '../styles/style.js'
-import Image from 'next/image';
+import Link from "../src/components/Link/NextLink";
+import {
+  Header,
+  HeaderImages,
+  Form,
+  FormWine,
+  PriceOpt,
+  Container,
+} from "../styles/style.js";
+import Image from "next/image";
 
-export default function Home() {
+const defaultEndpoint = "https://wine-back-test.herokuapp.com/products/";
+
+export async function getStaticProps() {
+  const response = await fetch(defaultEndpoint);
+  const data = await response.json();
+  return {
+    props: { data },
+  };
+}
+
+export default function Home({ data }) {
+  const { items = [] } = data;
   return (
     <div>
       <Header>
-        <Image 
-        src='/images/winelogo.png'
-        width={120}
-        height={40}
-        />
-        <Link href="/clube">
-          Clube
-        </Link>
-        <Link href="/loja">
-          Loja
-        </Link>
-        <Link href="/produtores">
-          Produtores
-        </Link>
-        <Link href="/ofertas">
-          Ofertas
-        </Link>
-        <Link href="/eventos">
-          Eventos
-        </Link>
+        <Image src="/images/winelogo.png" width={120} height={40} />
+        <Link href="/clube">Clube</Link>
+        <Link href="/loja">Loja</Link>
+        <Link href="/produtores">Produtores</Link>
+        <Link href="/ofertas">Ofertas</Link>
+        <Link href="/eventos">Eventos</Link>
         <HeaderImages>
-          <Image 
-            src='/images/lupa.png'
-            width={50}
-            height={50}
-          />
-          <Image 
-            src='/images/user.png'
-            width={50}
-            height={50}
-          />
-          <Image 
-            src='/images/carrinho.png'
-            width={50}
-            height={50}
-          />
+          <Image src="/images/lupa.png" width={50} height={50} />
+          <Image src="/images/user.png" width={50} height={50} />
+          <Image src="/images/carrinho.png" width={50} height={50} />
         </HeaderImages>
       </Header>
+      <Container>
+      <PriceOpt>
+        <h3>Refine sua Busca</h3>
+        <h4>Por preço</h4>
+        <label>
+          <input type="checkbox" /> Até R$40
+        </label>
+        <label>
+          <input type="checkbox" /> R$40 a R$60
+        </label>
+        <label>
+          <input type="checkbox" /> R$100 a R$200
+        </label>
+        <label>
+          <input type="checkbox" /> R$100 a R$200
+        </label>
+        <label>
+          <input type="checkbox" /> Acima de R$500
+        </label>
+      </PriceOpt>
+      <Form>
+        {items.map((item) => {
+          const { id, image, name, price, priceMember, priceNonMember } = item;
+          return (
+            <FormWine key={id}>
+              <img src={image} width="100px" alt="" />
+              <h4>{name}</h4>
+              <p>{price}</p>
+              <h4>Sócio Wine R${priceMember}</h4>
+              <h4>NÃO SÓCIO R${priceNonMember}</h4>
+                <button>Adicionar</button>
+            </FormWine>
+          );
+        })}
+      </Form>
+      </Container>
     </div>
-  )
+  );
 }
--- styles/style.js
+++ styles/style.js
@@ -18,6 +18,7 @@ export const Header = styled.div`
   font-weight: 400;
   font-size: 18px;
   line-height: 24px;
+  background-color: #fff;
   `
 
 export const HeaderImages = styled.div`
@@ -25,4 +26,69 @@ export const HeaderImages = styled.div`
   justify-content: space-around;
   flex-direction: row;
   width: 18%;
+`
+
+export const Form = styled.div`
+  background: #F5F5F5;
+  padding: 41px 30px 46px 41px;
+  display: flex;
+  flex-direction: row;
+  flex-flow: row wrap;
+  margin: auto;
+  justify-content: space-around;
+  width: 70%;
+`
+
+export const FormWine = styled.div`
+  margin-top: 100px;
+  background: #fff 0 0 no-repeat padding-box;
+  border: 0.5px;
+  border-radius: 5px;
+  box-shadow: 0px 9.73384px 14.6008px rgba(0, 0, 0, 0.1);
+  max-width: 270px;
+  min-width: 270px;
+  opacity: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 5px;
+  margin-right: 5px;
+
+  h4 {
+    text-align: center;
+  }
+
+  button {
+    position: absolute;
+    margin-top: 420px;
+    width: 271px;
+    background-color: #7ebc43;
+    border-color: #7ebc43;
+    color: #fff;
+    font-size: 16px;
+    height: 48px;
+    border-radius: 4px;
+    border-style: solid;
+    border-width: 2px;
+    cursor: pointer;
+    font-weight: 700;
+    text-align: center;
+  }
+`
+export const PriceOpt = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 150px;
+  background: #F5F5F5;
+  margin-left: 66px;
+  margin-top: 80px;
+
+  h3 {
+    width: 156px;
+    margin-bottom: 0px;
+  }
+`
+
+export const Container = styled.div`
+  display: flex;
 `
