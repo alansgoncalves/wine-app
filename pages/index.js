0b21ad308758ff3dd9ff4a005830c491555604a5
alansgoncalves <vekio.soft@gmail.com>
2022-07-03 11:57:08 -0300

    feat: build Header main page

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,49 @@
 import Link from '../src/components/Link/NextLink'
+import { Header, HeaderImages } from '../styles/style.js'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
-      <p>Winne Home</p>
-      <Link href="/clube">
-        Clube
-      </Link>
-      <Link href="/loja">
-        Loja
-      </Link>
-      <Link href="/produtores">
-        Produtores
-      </Link>
-      <Link href="/ofertas">
-        Ofertas
-      </Link>
-      <Link href="/eventos">
-        Eventos
-      </Link>
+      <Header>
+        <Image 
+        src='/images/winelogo.png'
+        width={120}
+        height={40}
+        />
+        <Link href="/clube">
+          Clube
+        </Link>
+        <Link href="/loja">
+          Loja
+        </Link>
+        <Link href="/produtores">
+          Produtores
+        </Link>
+        <Link href="/ofertas">
+          Ofertas
+        </Link>
+        <Link href="/eventos">
+          Eventos
+        </Link>
+        <HeaderImages>
+          <Image 
+            src='/images/lupa.png'
+            width={50}
+            height={50}
+          />
+          <Image 
+            src='/images/user.png'
+            width={50}
+            height={50}
+          />
+          <Image 
+            src='/images/carrinho.png'
+            width={50}
+            height={50}
+          />
+        </HeaderImages>
+      </Header>
     </div>
   )
 }
---
+++ styles/style.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  width: 100%;
+  box-sizing: border-box;
+  border: 0.5px solid #CCCCCC;
+  align-items: center;
+  justify-content: space-around;
+  box-shadow: 0px 2px 2px #CCCCCC;
+  height: 90px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
+  `
+
+export const HeaderImages = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-direction: row;
+  width: 18%;
+`
